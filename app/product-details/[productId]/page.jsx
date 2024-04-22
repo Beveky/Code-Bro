806f9eaf6a8ca604b6c9265f54b85d266a57e488
Beveky <im.beveky@gmail.com>
2024-04-22 23:00:43 +0100

    lastcommit

--- app/product-details/[productId]/page.jsx
+++ app/product-details/[productId]/page.jsx
@@ -1,5 +1,4 @@
 "use client";
-
 import ProductApis from "../../_utils/ProductApis";
 import React, { useEffect, useState } from "react";
 import ProductBanner from "./_components/ProductBanner";
@@ -8,42 +7,41 @@ import ProductList from "../../_components/ProductList";
 import { usePathname } from "next/navigation";
 import BreadCrumb from "../../_components/BreadCrumb";
 
-export default function ProductDetails({ params }) {
+function ProductDetails({ params }) {
   const path = usePathname();
   const [productDetails, setProductDetails] = useState({});
   const [productList, setProductList] = useState([]);
   useEffect(() => {
-    return () => {
-      getProductById_();
-    };
+    getProductById_();
   }, [params?.productId]);
-
   const getProductById_ = () => {
     ProductApis.getProductById(params?.productId).then((res) => {
-      console.log("product item", res.data.data);
+      console.log("product item ", res.data.data);
       setProductDetails(res.data.data);
       getProductByCategory(res.data.data);
     });
   };
-
   const getProductByCategory = (product) => {
-    ProductApis.getProductByCategory(product?.attributes?.category).then(
+    ProductApis.getProductByCategory(product?.attributes.category).then(
       (res) => {
         console.log(res?.data?.data);
         setProductList(res?.data?.data);
       }
     );
   };
-
   return (
     <div className="px-10 py-8 md:px-28">
       <BreadCrumb path={path} />
-      <div className="grid grid-cols-1 gap-10 justify-around mt-10 sm:grid-cols-2 sm:gap-5 ">
+      <div className="grid justify-around grid-cols-1 gap-5 mt-10 sm:gap-0 sm:grid-cols-2">
         <ProductBanner product={productDetails} />
         <ProductInfo product={productDetails} />
       </div>
-      <h2 className="mt-24 text-xl font-extrabold mb-4">Similar Products</h2>
-      <ProductList productList={productList} />
+      <div>
+        <h2 className="mt-24 mb-4 text-xl">Similar Products</h2>
+        <ProductList productList={productList} />
+      </div>
     </div>
   );
 }
+
+export default ProductDetails;

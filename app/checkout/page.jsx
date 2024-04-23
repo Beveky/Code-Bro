bebe4119223b2874143f999bcfc6991e2d9f1f7e
Beveky <im.beveky@gmail.com>
2024-04-23 19:42:48 +0100

    lastcommit

--- app/checkout/page.jsx
+++ app/checkout/page.jsx
@@ -5,6 +5,19 @@ export default function checkout() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Checkoutt />
+      {/* DemoCart-Test */}
+      <div className="flex flex-col gap-5 mx-auto max-w-screen-xl px-4 py-16  lg:flex lg:h-full">
+        <h2 className="text-hover font-bold text-3xl">Demo Cart For Test :</h2>
+        <h2 className="text-hover font-medium text-xl">
+          <span className="text-black">Cart Number :</span> 4242 4242 4242 4242
+        </h2>
+        <h2 className="text-hover font-medium text-xl">
+          <span className="text-black">Expiration Date :</span> 12/45
+        </h2>
+        <h2 className="text-hover font-medium text-xl">
+          <span className="text-black">CVC :</span> 335
+        </h2>
+      </div>
     </Suspense>
   );
 }
